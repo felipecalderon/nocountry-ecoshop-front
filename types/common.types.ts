95da445591a69968d85d460d119b5cfa1a03d03b
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-21 15:44:10 -0300

    feat: adds common.types.ts to types/

---
+++ types/common.types.ts
@@ -0,0 +1,24 @@
+export interface IconProps {
+  className?: string
+}
+
+export interface SocialIconLinkProps {
+  href: string
+  IconComponent: React.FC<IconProps>
+  label: string
+  hoverClass: string
+}
+
+export interface SectionLink {
+  href: string
+  label: string
+}
+
+export interface SectionColumn {
+  title: string
+  links: SectionLink[]
+}
+
+export interface ItemLinkColumnProps {
+  column: SectionColumn
+}
