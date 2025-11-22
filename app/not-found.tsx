ee521e247bca187ccbb0e4ae59fde73fbbb7e971
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-22 15:02:31 -0300

    feat: adds BackButton and modifies styles in app/not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,7 @@
-"use client"
-
 import Link from "next/link"
 import { Button } from "./components/ui/button"
-import { SearchX, Home, ArrowLeft } from "lucide-react"
+import { SearchX, Home } from "lucide-react"
+import { BackButton } from "./components/ui/BackButton"
 
 export default function NotFound() {
   return (
@@ -10,7 +9,7 @@ export default function NotFound() {
       <article className="text-center space-y-4 max-w-md">
         <header className="flex flex-col">
           <SearchX className="h-20 w-20 text-muted-foreground dark:text-gray-200 mx-auto" />
-          <h1 className="text-9xl font-bold text-muted-foreground dark:text-gray-200">
+          <h1 className="text-8xl font-bold text-muted-foreground dark:text-gray-200">
             404
           </h1>
         </header>
@@ -27,7 +26,7 @@ export default function NotFound() {
         <footer className="flex flex-col sm:flex-row gap-3 justify-center pt-4 [&>button]:cursor-pointer">
           <Button
             asChild
-            className="dark:bg-secondary dark:text-primary dark:hover:bg-secondary/80 dark:hover:text-primary/80"
+            className="dark:bg-secondary dark:hover:bg-secondary/80 dark:hover:text-primary/80"
           >
             <Link href="/">
               <Home className="mr-2 h-4 w-4" />
@@ -35,14 +34,7 @@ export default function NotFound() {
             </Link>
           </Button>
 
-          <Button
-            className="dark:hover:text-white"
-            variant="outline"
-            onClick={() => window.history.back()}
-          >
-            <ArrowLeft className="mr-2 h-4 w-4" />
-            Volver atrás
-          </Button>
+          <BackButton />
         </footer>
       </article>
     </section>
