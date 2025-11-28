417e9e8746cf2e5f0fbf9b3070894fb9e79cad84
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-28 12:29:47 -0300

    feat: adds carbonFootprint.types.ts to types/

---
+++ types/carbonFootprint.types.ts
@@ -0,0 +1,20 @@
+interface CarbonFootprintBreakdown {
+  productId: string
+  productName: string
+  quantity: number
+  co2PerUnit: number
+  waterPerUnit: number
+  subtotalCO2: number
+  subtotalWater: number
+}
+
+export interface CarbonFootprintResult {
+  totalCO2: number
+  totalWater: number
+  breakdown: CarbonFootprintBreakdown[]
+}
+
+export interface CarbonFootprintDisplayProps {
+  footprint: CarbonFootprintResult
+  showBreakdown?: boolean
+}
