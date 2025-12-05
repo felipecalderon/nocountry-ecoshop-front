c95a1faff5a08e202e71ce9204ab75f0bb9e6110
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-05 16:56:30 -0300

    refactor: reorganizes HTML structure and adjusts styles in app/components/addresses/AddessesCards.tsx

--- app/components/addresses/AddessesCards.tsx
+++ app/components/addresses/AddessesCards.tsx
@@ -1,6 +1,8 @@
 "use client"
+
+import { useState } from "react"
+import { Eye, Trash2 } from "lucide-react"
 import { cn } from "@/lib/utils"
-import { Address } from "@/types"
 import {
   addressTypeColors,
   addressTypeIcons,
@@ -8,9 +10,7 @@ import {
 } from "@/lib/data/addressItems"
 import { Badge } from "../ui/badge"
 import { Button } from "../ui/button"
-import { Eye, Loader2, Trash2 } from "lucide-react"
 import DeleteConfirmationModal from "./DeleteConfirmationModal"
-import { useState } from "react"
 import AddressCardDetails from "./AddressCardDetails"
 import {
   Accordion,
@@ -18,18 +18,15 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "../ui/accordion"
+import { Address } from "@/types"
 
-interface DashboardCardProps {
-  addresses: Address[]
-}
-
-export default function AddessesCards({ addresses }: DashboardCardProps) {
+export default function AddessesCards({ addresses }: { addresses: Address[] }) {
   const [selectedAddressId, setSelectedAddressId] = useState<string | null>(
     null
   )
 
   return (
-    <div
+    <section
       className={cn(
         "bg-card rounded-lg border border-border shadow-sm overflow-hidden"
       )}
@@ -39,69 +36,83 @@ export default function AddessesCards({ addresses }: DashboardCardProps) {
           Lista de direcciones
         </h3>
       </div>
-      <div className="p-6">
-        <div className="space-y-4">
-          {addresses.map((address) => {
-            const Icon = addressTypeIcons[address.addressType]
 
-            return (
-              <article
-                key={address.id}
-                className="border border-border rounded-lg p-6 hover:shadow-md transition-shadow"
-              >
-                <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-4 mb-4">
-                  <aside className="flex items-center gap-4">
+      <div className="space-y-4 p-6">
+        {addresses.map((address) => {
+          const Icon = addressTypeIcons[address.addressType]
+
+          return (
+            <article
+              key={address.id}
+              className="border border-border rounded-lg p-6 hover:shadow-md transition-shadow"
+            >
+              <div className="flex flex-col sm:flex-row md:items-center justify-between gap-4 mb-4">
+                <section className="flex justify-between gap-4">
+                  <div className="flex items-center gap-2">
                     <figure className="shrink-0 w-12 h-12 bg-primary/10 dark:bg-secondary rounded-lg flex items-center justify-center">
                       <Icon className="h-6 w-6 text-primary" />
                     </figure>
-                    <aside>
-                      <p className="font-semibold text-lg text-foreground">
+                    <div>
+                      <p className="font-semibold sm:text-lg text-foreground">
                         {address.street}
                       </p>
                       <p className="text-sm text-muted-foreground">
                         {address.city}, {address.postalCode}
                       </p>
-                    </aside>
-                  </aside>
-                  <Badge className={addressTypeColors[address.addressType]}>
+                    </div>
+                  </div>
+
+                  <div className="sm:hidden">
+                    <Badge className={addressTypeColors[address.addressType]}>
+                      {addressTypeLabels[address.addressType]}
+                    </Badge>
+                  </div>
+                </section>
+
+                <aside className="flex flex-col sm:items-end gap-2">
+                  <Badge
+                    className={`hidden sm:flex ${
+                      addressTypeColors[address.addressType]
+                    }`}
+                  >
                     {addressTypeLabels[address.addressType]}
                   </Badge>
-                </div>
 
-                <div className="flex flex-wrap gap-2 [&>button]:cursor-pointer">
                   <Button
                     variant="outline"
                     size="sm"
                     onClick={() => setSelectedAddressId(address.id)}
-                    className="hover:text-red-700 hover:bg-red-50 dark:hover:bg-red-900/20 dark:hover:text-red-100"
+                    className="hover:text-red-700 hover:bg-red-50 dark:hover:bg-neutral-900/60 dark:hover:text-red-100 cursor-pointer"
                   >
                     <Trash2 className="h-4 w-4 mr-2" />
                     Eliminar
                   </Button>
-                  <Accordion type="single" collapsible>
-                    <AccordionItem value={address.id}>
-                      <AccordionTrigger>
-                        <Button className="w-full">
-                          <Eye className="h-4 w-4 mr-2" />
-                          Ver detalles
-                        </Button>
-                      </AccordionTrigger>
-                      <AccordionContent>
-                        <AddressCardDetails address={address} />
-                      </AccordionContent>
-                    </AccordionItem>
-                  </Accordion>
-                </div>
-                <DeleteConfirmationModal
-                  isOpen={selectedAddressId === address.id}
-                  onClose={() => setSelectedAddressId(null)}
-                  address={address}
-                />
-              </article>
-            )
-          })}
-        </div>
+                </aside>
+              </div>
+
+              <Accordion type="single" collapsible className="border-0">
+                <AccordionItem value={address.id} className="border-0">
+                  <AccordionTrigger className="bg-gray-50 hover:bg-gray-100 dark:bg-neutral-900/10 dark:hover:bg-neutral-900/20 hover:no-underline py-2 px-4 cursor-pointer">
+                    <span className="flex items-center text-sm font-medium">
+                      <Eye className="h-4 w-4 mr-2" />
+                      Ver detalles
+                    </span>
+                  </AccordionTrigger>
+                  <AccordionContent className="pt-3 mb-0 pb-0">
+                    <AddressCardDetails address={address} />
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
+
+              <DeleteConfirmationModal
+                isOpen={selectedAddressId === address.id}
+                onClose={() => setSelectedAddressId(null)}
+                address={address}
+              />
+            </article>
+          )
+        })}
       </div>
-    </div>
+    </section>
   )
 }
