437e11178eb3d18e42fe5671146afe062fe869cb
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-09 08:54:09 -0300

    refactor: removes types imports in actions/payments.ts

--- actions/payments.ts
+++ actions/payments.ts
@@ -1,8 +1,7 @@
 import { fetcher } from "@/lib/fetcher"
-import { CreateCheckoutSessionDto } from "@/types"
 import { auth0 } from "@/lib/auth0"
 
-export const createCheckoutSession = async (data: CreateCheckoutSessionDto) => {
+export const createCheckoutSession = async (data: { orderId: string }) => {
   try {
     const { token } = await auth0.getAccessToken()
     return await fetcher("POST", "/payments/create-checkout-session", {
