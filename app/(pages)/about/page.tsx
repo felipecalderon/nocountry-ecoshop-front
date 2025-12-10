8cb337247853c14a2f8760e06793c09b2400562c
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-12-10 18:02:47 -0300

    feat: adds StorySection, storySections and OurTeam and updates structure in app/(pages)/about/page.tsx

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,3 +1,26 @@
+import StorySection from "@/app/components/ui/StorySection"
+import { storySections } from "@/lib/data/storySections"
+import OurTeam from "@/app/components/ui/OurTeam"
+
 export default function About() {
-  return <p>Aboutt</p>
+  return (
+    <section className="container mx-auto px-4 pb-8">
+      <article className="max-w-5xl mx-auto">
+        <section className="space-y-24 py-12 xl:space-y-32">
+          {storySections.map((section, index) => (
+            <StorySection
+              key={index}
+              title={section.title}
+              subtitle={section.subtitle}
+              description={section.description}
+              imageSrc={section.imageSrc}
+              imageAlt={section.imageAlt}
+              reverse={section.reverse}
+            />
+          ))}
+        </section>
+        <OurTeam />
+      </article>
+    </section>
+  )
 }
